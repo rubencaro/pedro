c3f36c06616c8c029a5c183cfbd5ff9c9c4a4885
Rubén Caro <ruben.caro.estevez@gmail.com>
2016-06-05 20:44:14 +0200

    Add some docs

--- lib/pedro/mnesia.ex
+++ lib/pedro/mnesia.ex
@@ -2,6 +2,15 @@ require Pedro.Helpers, as: H
 
 defmodule Pedro.Mnesia do
 
+  @moduledoc """
+  Helpers to interact with Mnesia in single node mode.
+  """
+
+  @doc """
+  create_schema and then start Mnesia in single node mode
+
+  Returns `:ok` or `{:error, reason}`
+  """
   def init() do
     case :mnesia.create_schema([node()]) do
       :ok -> :ok
@@ -12,6 +21,12 @@ defmodule Pedro.Mnesia do
     :mnesia.start
   end
 
+  @doc """
+  Create a table with given `name` and options.
+  `:attributes` are required.
+
+  Returns `{:ok, :atomic}` or `{:aborted, reason}`
+  """
   def create_table(name, opts \\ []) when is_atom(name) do
     opts = opts
       |> H.requires([:attributes])
