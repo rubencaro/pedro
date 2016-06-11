6cfab5ae4e41eda1e6ba952f906cfe385118e157
Rubén Caro <ruben.caro.estevez@gmail.com>
2016-06-11 15:34:36 +0200

    Mnesia initalization

--- lib/pedro/mnesia.ex
+++ lib/pedro/mnesia.ex
@@ -12,42 +12,50 @@ defmodule Pedro.Mnesia do
   Returns `:ok` or `{:error, reason}`
   """
   def init() do
-    with :ok <- create_schema,
-         :ok <- :mnesia.start,
-         true <- create_all_tables,
-         do: :ok
+    create_schema
+    |> start_mnesia
+    |> create_all_tables
   end
 
   defp create_schema do
     case :mnesia.create_schema([node()]) do
       :ok -> :ok
       {:error, {_, {:already_exists, _}}} -> :ok
-      any -> raise "Error initialising mnesia: #{inspect any}"
+      any -> any
     end
   end
 
-  defp create_all_tables do
+  defp start_mnesia(:ok), do: :mnesia.start
+  defp start_mnesia(any), do: any  # error piping
+
+  defp create_all_tables(:ok) do
+
+    results = tables_definition |> Enum.map(&create_table/1)
+
+    case Enum.all?(results, &(match?(:ok,&1))) do
+      true -> :ok
+      false -> {:error, results}
+    end
+  end
+  defp create_all_tables(any), do: any  # error piping
+
+  defp create_table([name: name, opts: opts]) when is_atom(name) do
+    opts = opts
+      |> H.requires([:attributes])
+      |> H.defaults(disc_copies: [node()])
+
+    case :mnesia.create_table(name, opts) do
+      {:ok, :atomic} -> :ok
+      {:aborted, {:already_exists, _}} -> :ok
+      {:aborted, reason} -> {:error, reason}
+    end
+  end
+
+  defp tables_definition do
     [
       [name: Pedro.EntryQueue, opts: [attributes: [:id, :received_ts, :target_ts, :adapter, :options, :json_payload]]],
       [name: Pedro.Messages,   opts: [attributes: [:id, :received_ts, :target_ts, :deliver_ts, :json_payload]]],
       [name: Pedro.Throttles,  opts: [attributes: [:key, :since_ts, :consumed, :period]]]
     ]
-    |> Enum.map(&create_table/1)
-    |> Enum.all?(&(match?({:ok,:atomic},&1)))
   end
-
-  @doc """
-  Create a table with given `name` and options.
-  `:attributes` are required.
-
-  Returns `{:ok, :atomic}` or `{:aborted, reason}`
-  """
-  def create_table([name: name, opts: opts]) when is_atom(name) do
-    opts = opts
-      |> H.requires([:attributes])
-      |> H.defaults(disc_copies: [node()])
-
-    :mnesia.create_table(name, opts)
-  end
-
 end
